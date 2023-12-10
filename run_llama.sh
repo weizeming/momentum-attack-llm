f51c4645a679dfb40cecd666505a1e43db733d2c
Zeming Wei <weizeming@stu.pku.edu.cn>
2023-12-09 16:06:36 -0800

    Delete run_llama.sh

--- run_llama.sh
+++
@@ -1,5 +0,0 @@
-python gcg.py --save-name 'llama_mgcg_01' --model-path /root/autodl-tmp/models/Llama-2-7b-chat-hf --steps 20 --mu 0.8
-python gcg.py --save-name 'llama_mgcg_02' --model-path /root/autodl-tmp/models/Llama-2-7b-chat-hf --steps 20 --mu 0.8
-python gcg.py --save-name 'llama_mgcg_03' --model-path /root/autodl-tmp/models/Llama-2-7b-chat-hf --steps 20 --mu 0.8
-python gcg.py --save-name 'llama_mgcg_04' --model-path /root/autodl-tmp/models/Llama-2-7b-chat-hf --steps 20 --mu 0.8
-python gcg.py --save-name 'llama_mgcg_05' --model-path /root/autodl-tmp/models/Llama-2-7b-chat-hf --steps 20 --mu 0.8
